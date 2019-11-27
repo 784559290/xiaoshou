a189458e20a48144b47d1d3551fc3bf75bf74d60
xiaoshuo <784559290@qq.com>
2019-11-27 13:39:39 +0800

    666

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
     chainWebpack: (config) => {
         config.resolve.alias
             .set('@', resolve('src'))
-            .set('assets',resolve('src/assets'))
+            .set('@assets',resolve('src/assets'))
             .set('@views',resolve('src/views'))
             .set('@components',resolve('src/components'))
             .set('@mutations',resolve('src/store/mutations-types.js'))
